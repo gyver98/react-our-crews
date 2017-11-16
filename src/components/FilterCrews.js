3e85340555e9b0709fd7b8f28f86667544db8f4f
Matthew Choi <matthewchoi7@gmail.com>
2017-11-16 16:58:32 +1100

    updating react-our-crews

--- src/components/FilterCrews.js
+++ src/components/FilterCrews.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 class FilterCrews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterAll: true,
+      filterMel: false,
+      filterSyd: false
+    }
+    this._toggleFilterActive = this._toggleFilterActive.bind(this);
+
+  }
+
+_toggleFilterActive() {
+  this.setState(prevState => ({
+    filterAll: !prevState.filterAll,
+    filterMel: !prevState.filterMel,
+    filterSyd: !prevState.filterSyd
+  }))
+}
+
+
   render() {
     return (
       <ul className="filter-member">
-        <li><button className="filter-member__option active" data-filtermember>All</button></li>
-        <li><button className="filter-member__option" data-filtermember="melbourne-active">Melbourne</button></li>
-        <li><button className="filter-member__option" data-filtermember="sydney-active">Sydney</button></li>
+        <li><button onClick={this._toggleFilterActive} className={classNames("filter-member__option", {"active": this.state.filterAll})} data-filtermember>All</button></li>
+        <li><button onClick={this._toggleFilterActive} className={classNames("filter-member__option", {"active": this.state.filterMel})} data-filtermember="melbourne-active">Melbourne</button></li>
+        <li><button onClick={this._toggleFilterActive} className={classNames("filter-member__option", {"active": this.state.filterSyd})} data-filtermember="sydney-active">Sydney</button></li>
       </ul>
     )
   }
