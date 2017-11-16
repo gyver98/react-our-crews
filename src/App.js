3243bf6c6ce6ec1def7f9f811de1cf56581a3f80
Matthew Choi <matthewchoi7@gmail.com>
2017-11-16 11:28:33 +1100

    creating react-our-crews

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="contents">
+        <h2 className="pagetitle">
+          AI FED crews
+        </h2>
+        <main>
+          <ul className="filter-member">
+            <li><button className="filter-member__option active" data-filtermember>All</button></li>
+            <li><button className="filter-member__option" data-filtermember="melbourne-active">Melbourne</button></li>
+            <li><button className="filter-member__option" data-filtermember="sydney-active">Sydney</button></li>
+          </ul>
+          <section className="grid-container" data-filteractive>
+            <div className="box crew-photo i1" data-track="melbourne">
+              <img src="img/luke.peary.jpg" alt="" />
+            </div>
+            <div className="box profile-hover c1" data-track="melbourne">
+              <div className="crew-profile front">
+                <h1 className="crew-profile__name">Luke Peary</h1>
+                <span className="crew-profile__title">Front End Development Specialist</span>
+                <p className="crew-profile__area">Melbourne</p>
+              </div>
+              <div className="crew-profile back">
+                <h1 className="crew-profile__name">Guess who?</h1>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>    
+    );
+  }
+}
+
+export default App;
