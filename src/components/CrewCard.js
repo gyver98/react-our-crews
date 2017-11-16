fe2df4ba57c63ad55da2b0e0d9ccb238610bde43
Matthew Choi <matthewchoi7@gmail.com>
2017-11-16 14:59:57 +1100

    updating react-our-crews

--- src/components/CrewCard.js
+++ src/components/CrewCard.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 
 class CrewCard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      flipCard: false
+    }
+    this._addFlipClass = this._addFlipClass.bind(this);
+    this._removeFlipClass = this._removeFlipClass.bind(this);
+  }
+  /* Using classnames library
+   Replacement Jquery hover handling code below
+   $('.profile-hover').hover(function(){
+        $(this).addClass('flip');
+      },function(){
+        $(this).removeClass('flip');
+      });
+  */
+  _addFlipClass() {
+    this.setState({
+      flipCard: true
+    })
+  }
+
+  _removeFlipClass() {
+    this.setState({
+      flipCard: false
+    })
   }
 
   render() {
@@ -11,7 +36,10 @@ class CrewCard extends Component {
         <div className={`box crew-photo i${this.props.id}`} data-track="melbourne">
           <img src={require('../img/asset 2.jpeg')} alt="" />
         </div>,
-        <div className={`box profile-hover c${this.props.id}`} data-track="melbourne">
+        <div 
+          onMouseEnter={this._addFlipClass} 
+          onMouseLeave={this._removeFlipClass}
+          className={classNames(`box profile-hover c${this.props.id}`, {"flip": this.state.flipCard})} data-track="melbourne">
           <div className="crew-profile front">
             <h1 className="crew-profile__name">{this.props.name}</h1>
             <span className="crew-profile__title">{this.props.title}</span>
